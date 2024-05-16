f45968e7ab532bb54e2b58579595834857421d9c
daniecce <157365622+daniecce@users.noreply.github.com>
2024-05-16 09:40:26 +0200

    Create match_stats.sql

---
+++ Create_database/match_stats.sql
@@ -0,0 +1,52 @@
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(1,1,12,2,54,580,1,1,0,11,5);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(1,2,18,5,46,501,4,1,0,9,4);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(2,1,31,11,76,705,3,2,0,7,13);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(2,3,4,3,24,237,2,4,0,12,0);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(3,1,23,10,58,725,1,0,0,2,6);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(3,4,7,4,42,528,2,1,0,7,4);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(4,1,13,6,55,640,0,2,0,10,4);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(4,4,13,5,45,548,1,1,0,15,1);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(5,1,17,8,70,737,1,1,0,12,5);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(5,3,12,5,30,302,2,1,0,11,3);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(6,1,15,5,51,564,0,1,0,5,5);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(6,2,17,3,49,538,0,2,1,10,7);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(7,1,3,0,42,510,0,4,0,12,1);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(7,5,21,8,58,677,1,4,0,14,7);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(8,1,21,7,44,448,2,4,0,12,5);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(8,5,10,4,56,590,2,4,0,9,2);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(9,1,8,5,42,466,1,1,0,6,5);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(9,6,20,5,58,611,0,1,0,14,2);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(10,1,10,4,43,581,0,4,0,17,1);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(10,6,28,7,57,746,1,4,0,20,10);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(11,1,11,5,40,416,2,1,0,5,3);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(11,7,16,6,60,613,3,1,0,11,5);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(12,1,14,5,44,532,3,4,0,14,3);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(12,7,15,10,56,659,3,3,0,18,9);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(13,1,4,2,46,514,4,0,0,7,2);
+insert into match_stats(match_id,team_id,shots,shots_on_target,possession_percentage,accurate_passes,offsides,yellow_cards,red_cards,fouls,corners) 
+	values(13,8,24,9,54,592,1,1,0,13,6);
