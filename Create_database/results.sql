e57bcafff2f42f4866f5d8cbf3cd9c7a8395be87
daniecce <157365622+daniecce@users.noreply.github.com>
2024-05-16 09:39:37 +0200

    Create results.sql

---
+++ Create_database/results.sql
@@ -0,0 +1,13 @@
+insert into results(match_id,home_score,away_score,winner) values (1,0,1,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (2,1,2,"Sheriff Tiraspol");
+insert into results(match_id,home_score,away_score,winner) values (3,0,5,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (4,2,1,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (5,0,3,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (6,2,0,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (7,1,0,"Paris Saint Germain");
+insert into results(match_id,home_score,away_score,winner) values (8,3,1,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (9,1,3,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (10,2,3,"Chelsea London");
+insert into results(match_id,home_score,away_score,winner) values (11,4,3,"Manchester City");
+insert into results(match_id,home_score,away_score,winner) values (12,3,1,"Real Madrid");
+insert into results(match_id,home_score,away_score,winner) values (13,0,1,"Real Madrid");
