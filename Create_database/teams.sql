2fcc2d32a9f5d54712c8e61d76dde0ccb4372224
daniecce <157365622+daniecce@users.noreply.github.com>
2024-05-16 09:37:20 +0200

    Create teams.sql
    Completing the 'teams' table

---
+++ Create_database/teams.sql
@@ -0,0 +1,8 @@
+insert into teams(team_id,team_name,league) values (1,"Real Madrid", "Champions League");
+insert into teams(team_id,team_name,league) values (2,"Inter Milan", "Champions League");
+insert into teams(team_id,team_name,league) values (3,"Sheriff Tiraspol", "Champions League");
+insert into teams(team_id,team_name,league) values (4,"Shakhtar Donetsk", "Champions League");
+insert into teams(team_id,team_name,league) values (5,"Paris Saint Germain", "Champions League");
+insert into teams(team_id,team_name,league) values (6,"Chelsea London", "Champions League");
+insert into teams(team_id,team_name,league) values (7,"Manchester City", "Champions League");
+insert into teams(team_id,team_name,league) values (8,"Liverpool", "Champions League");
